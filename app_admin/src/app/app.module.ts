55a359d18d4398b356f9833576dfc2eea0a37acf
GeoffNixCodes <fullstackgeoff@gmail.com>
2024-02-22 21:01:37 -0500

    Stuck

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -4,16 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
 import { TripCardComponent } from './trip-card/trip-card.component';
+import { LoginComponent } from './login/login.component';
+import { AddTripComponent } from './add-trip/add-trip.component';
+import { EditTripComponent } from './edit-trip/edit-trip.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     TripListingComponent,
-    TripCardComponent
+    TripCardComponent,
+    LoginComponent,
+    AddTripComponent,
+    EditTripComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
